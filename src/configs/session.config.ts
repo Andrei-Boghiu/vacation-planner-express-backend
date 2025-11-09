ded61dc3c28a1fc394e0699ac1bc6def35cf1da5
Andrew <vandrei1408@yahoo.com>
2025-11-10 00:49:57 +0200

    fix: qudo suggestions

--- src/configs/session.config.ts
+++ src/configs/session.config.ts
@@ -3,16 +3,19 @@ import { IS_PROD, SESSION_SECRET } from "./env.config";
 import { PrismaSessionStore } from "@quixo3/prisma-session-store";
 import prisma from "../prisma/prisma.client";
 
+const TTL = 60_000 * 60 * 8; // 8h
+
 const SESSION_OPTIONS: SessionOptions = {
   store: new PrismaSessionStore(prisma, {
-    checkPeriod: 2 * 60 * 1000,
+    checkPeriod: 60 * 60 * 1000,
     dbRecordIdIsSessionId: true,
+    ttl: TTL,
   }),
   secret: SESSION_SECRET,
   saveUninitialized: false,
   resave: false,
   cookie: {
-    maxAge: 60_000 * 60 * 8, // 8h
+    maxAge: TTL,
     httpOnly: true,
     sameSite: "lax",
     secure: IS_PROD,
