082a70688c126d170f0cce9fb9accacdbda46874
Andrew <124816994+Andrei-Boghiu@users.noreply.github.com>
2025-11-10 00:53:26 +0200

    feat: add persistent sessions (#5)
    * feat: add persistent sessions

--- src/configs/session.config.ts
+++ src/configs/session.config.ts
@@ -1,13 +1,23 @@
 import { SessionOptions } from "express-session";
 import { IS_PROD, SESSION_SECRET } from "./env.config";
+import { PrismaSessionStore } from "@quixo3/prisma-session-store";
+import prisma from "../prisma/prisma.client";
+
+const TTL = 60_000 * 60 * 8; // 8h
 
 const SESSION_OPTIONS: SessionOptions = {
+  store: new PrismaSessionStore(prisma, {
+    checkPeriod: 60 * 60 * 1000,
+    dbRecordIdIsSessionId: true,
+    ttl: TTL,
+  }),
   secret: SESSION_SECRET,
   saveUninitialized: false,
   resave: false,
   cookie: {
-    maxAge: 60_000 * 60 * 8, // 8h
+    maxAge: TTL,
     httpOnly: true,
+    sameSite: "lax",
     secure: IS_PROD,
   },
 };
